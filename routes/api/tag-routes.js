d340f8e04de2521dbe3afa69f9dd630963ea024a
DanielWestiner <daniel.westiner@gmail.com>
2022-01-26 22:52:06 -0500

    completed tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -9,9 +9,9 @@ router.get('/', (req, res) => {
   Tag.findAll({
     include: [
       {
-        model:Product,
+        model: Product,
         as: "products",
-        attributes: ["product_name", "price", "stock", "category_id", "product_tag"]
+        attributes: ["product_name", "price", "stock"]
       }
     ]
   })
@@ -25,19 +25,87 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        through: "prodcuts",
+        as: "products",
+        attributes: ["product_name", "price", "stock"]
+      }
+    ]
+  })
+  .then(data => {
+    if(!data) {
+      res.status(404).json({ message: "No ID Found" });
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(data => res.json(data))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+      id: req.params.id
+      }
+    }
+  )
+  .then(data => {
+    if(!data) {
+      res.status(404).json({ message: "No ID Found!" });
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if(!data) {
+      res.status(404).json({ message: "No ID Found" });
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.statuus(500).json(err);
+  });
 });
 
 module.exports = router;
